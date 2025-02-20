c6eb11dc52d215e0c63820654eff115970da8f18
Yang Zhou <yangzhouinter@gmail.com>
2025-02-21 01:25:37 +1100

    create dynamic background for the top banner in Recomend page

--- src/views/discover/child-views/Recommend/child-components/top-banner/index.tsx
+++ src/views/discover/child-views/Recommend/child-components/top-banner/index.tsx
@@ -1,8 +1,9 @@
 import { useAppSelector } from '@/hooks/useAppSelector';
-import { memo } from 'react';
+import { memo, useRef, useState } from 'react';
 import type { FC, ReactNode } from 'react';
 import { shallowEqual } from 'react-redux';
 import { Carousel } from 'antd';
+import type { CarouselRef } from 'antd/es/carousel';
 import { BannerControl, BannerLeft, BannerRight, BannerWrapper } from './style';
 
 interface IProps {
@@ -11,6 +12,11 @@ interface IProps {
 }
 
 const TopBanner: FC<IProps> = () => {
+  const [currentBannerIndex, setCurrentBannerIndex] = useState(0);
+
+  // antd Carousel has its own ref type definition which can be imported from 'antd/es/carousel'
+  const bannerRef = useRef<CarouselRef>(null);
+
   // get banner data from store
   const { banners } = useAppSelector((state) => {
     return {
@@ -18,14 +24,38 @@ const TopBanner: FC<IProps> = () => {
     };
   }, shallowEqual);
 
-  function handlePrevClick() {}
-  function handleNextClick() {}
+  // Carousel afterChange callback function
+  function handleAfterChange(current: number) {
+    // Carousel afterChange will pass the current index of the banner to the callback function
+    setCurrentBannerIndex(current);
+  }
+
+  function handlePrevClick() {
+    bannerRef.current?.prev();
+  }
+  function handleNextClick() {
+    bannerRef.current?.next();
+  }
+
+  let bgImageUrl = banners[currentBannerIndex]?.imageUrl; // currentBannerIndex could be undefined when banner has not been loaded
+  // add blur effect to the background image
+  if (bgImageUrl) {
+    bgImageUrl = bgImageUrl + '?imageView&blur=40x20';
+  }
 
   return (
-    <BannerWrapper>
+    <BannerWrapper
+      style={{ background: `url(${bgImageUrl}) center center/6000px` }}
+    >
       <div className="banner wrap-v2">
         <BannerLeft>
-          <Carousel autoplay autoplaySpeed={2000} effect="fade">
+          <Carousel
+            autoplay
+            autoplaySpeed={2000}
+            effect="fade"
+            ref={bannerRef}
+            afterChange={handleAfterChange}
+          >
             {banners.map((item) => {
               return (
                 <div className="banner-item" key={item.imageUrl}>
