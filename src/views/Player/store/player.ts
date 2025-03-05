41fb881d6069ce89b21f33f5e02a9a8fbc9a9336
Yang Zhou <yangzhouinter@gmail.com>
2025-03-06 02:27:18 +1100

    search the song in the playlist before playing. If the song information exists in the list, use it and dont fetch from server, then update the index of current playing song.

--- src/views/Player/store/player.ts
+++ src/views/Player/store/player.ts
@@ -1,21 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import type { ISong, ILyricLine } from '@/views/Player/type';
 import { getSongDetail, getSongLyric } from '../service/player';
 import { parseLyric } from '@/utils/parse-lyric';
+import type { RootStateType } from '@/store';
 
 import { songDetail01 } from '@/assets/data/api-data'; // replace this later with your api data
 import { songDetail02 } from '@/assets/data/api-data'; // replace this later with your api data
 
 // fetch current song information
-export const fetchCurrentSongAction = createAsyncThunk(
-  'player/fetchCurrentSong',
-  async (id: number) => {
+export const fetchCurrentSongAction = createAsyncThunk<
+  ISong,
+  number,
+  { state: RootStateType } // add the state type to the third argument
+>('player/fetchCurrentSong', async (id: number, { dispatch, getState }) => {
+  // check if the song is already in the playSongList before fetching it
+  const playSongList = getState().player.playSongList; // get state from the second argument
+  const playSongIndex = playSongList.findIndex((song) => song.id === id);
+  console.log(playSongIndex);
+  // if the song is not in the playSongList, fetch it from the server and return it
+  if (playSongIndex === -1) {
     const response = await getSongDetail(id);
     if (!response.songs.length) return;
     return response.songs[0];
-  },
-);
+  } else {
+    // if the song is in the playSongList, update the playSongIndex and return the song
+    dispatch(changePlaySongIndexAction(playSongIndex));
+    return playSongList[playSongIndex];
+  }
+});
 
 // fetch current song lyric
 export const fetchCurrentLyricAction = createAsyncThunk(
@@ -52,8 +64,18 @@ const playerSlice = createSlice({
   name: 'player',
   initialState: initialState,
   reducers: {
+    // record the index of the line of the lyric being played
     changeLyricIndexAction(state, { payload }) {
       state.lyricIndex = payload;
+    },
+    // record the index of the song being played
+    changePlaySongIndexAction(state, { payload }) {
+      console.log(payload);
+      state.playSongIndex = payload;
+    },
+    // update the playSongList
+    changePlaySongListAction(state, { payload }) {
+      state.playSongList = payload;
     },
   },
   extraReducers: (builder) => {
@@ -82,5 +104,12 @@ const playerSlice = createSlice({
   },
 });
 
-export const { changeLyricIndexAction } = playerSlice.actions;
+// export actions
+export const {
+  changeLyricIndexAction,
+  changePlaySongIndexAction,
+  changePlaySongListAction,
+} = playerSlice.actions;
+
+// export reducer to register in global store
 export default playerSlice.reducer;
